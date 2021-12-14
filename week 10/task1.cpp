3ed0e94837ea2398523343e1c2332d04ebe7dd74
RadoslavBotov <92144419+RadoslavBotov@users.noreply.github.com>
2021-12-14 21:40:08 +0200

    q

--- week 10/task1.cpp
+++ week 10/task1.cpp
@@ -1,10 +1,57 @@
-#include<iostream>
+// Напишете функция, която извежда всяка дума от даден символен низ.
+#include <iostream>
+#include <cstring>
 using namespace std;
+
+int findStartIndex(char *str, int index)
+{
+    int i = index;
+    while (!(str[i] > 'a' && str[i] < 'z') || !(str[i] > 'A' && str[i] > 'Z'))
+        i++;
+    return i;
+}
+
+int findEndIndex(char *str, int index)
+{
+    int i = index;
+    while ((str[i] > 'a' && str[i] < 'z') || (str[i] > 'A' && str[i] > 'Z'))
+        i++;
+    return i;
+}
+
+void printSubString(char *str, int start, int end)
+{
+    for (int i = start; i < end; i++)
+        cout << str[i];
+    cout << endl;
+}
+
+void wordsInString(char *str)
+{
+    int size = strlen(str);
+    int start = 0;
+    int end = 0;
+
+    while (end < size)
+    {
+        start = findStartIndex(str, end);
+        end = findEndIndex(str, start);
+
+        if ((start > size) || (end > size))
+            break;
+
+        printSubString(str, start, end);
+    }
+}
 
 int main()
 {
-    int a;
-    int b;
+    char str[100];
+
+    cout << "> Input: ";
+    cin.getline(str, 100);
+
+    wordsInString(str);
 
     return 0;
 }
